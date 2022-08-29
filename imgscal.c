d3b2e98a004dc2f024d0e73bf61f09b0f19215ae
Ali Mousa <ali.mousa.sy.at@gmail.com>
2022-08-29 11:11:52 +0200

    BEFORE IT WORKED only for P3 Format. NOW IT WORKS ONLY For P6 Format.

--- imgscal.c
+++ imgscal.c
@@ -11,9 +11,9 @@
 
 typedef struct
 {
-    double r;
-    double g;
-    double b;
+    char r;
+    char g;
+    char b;
 } pixel_rgb;
 
 
@@ -100,7 +100,7 @@ int main(int argc, char *argv[])
 
     pixel_rgb** img = NULL;
 
-    fscanf(fp1, "%c%d%d%d%d",&format,&format_2,&x,&y,&max);
+    fscanf(fp1, "%c%d\n%d%d%d\n",&format,&format_2,&x,&y,&max);
 
     img = calloc(1,(y+2)*sizeof(pixel_rgb*));
     for (int i = 0; i<(y+2); i++)
@@ -112,9 +112,9 @@ int main(int argc, char *argv[])
     {
         for(int k = 0; k<x; k++)
         {
-            fscanf(fp1,"%lf",&img[i][k].r);
-            fscanf(fp1,"%lf",&img[i][k].g);
-            fscanf(fp1,"%lf",&img[i][k].b);
+            fscanf(fp1,"%c",&img[i][k].r);
+            fscanf(fp1,"%c",&img[i][k].g);
+            fscanf(fp1,"%c",&img[i][k].b);
         }
     }
     FILE* fp2;
@@ -128,17 +128,26 @@ int main(int argc, char *argv[])
     
     pixel_rgb* img2 = NULL;
 
-    if (size>=1)
-    {
+    //if (size>=1)
+   // {
 
+
+	if (size>=1){
         img2 = calloc(1,ceil(x*size)*ceil(y*size)*sizeof(pixel_rgb));
+        
+        }
+        
+        else {
+        	img2 = calloc(1,floor(x*size*y)*sizeof(pixel_rgb));
+        }
+        
         fprintf(fp2,"%c%d\n%.0lf %.0lf\n%d\n",format,format_2,ceil(x*size),ceil(y*size),max);
 
         double old_x = 0;
         double old_y = 0;
 
-        double width_ratio = (double)(x)/(double)(x*size);
-        double height_ratio = (double) (y)/(double)(y*size);
+        double width_ratio = (double)(x)/(x*size);
+        double height_ratio = (double) (y)/(y*size);
 
         double A_r, A_g, A_b;
         double B_r, B_g, B_b;
@@ -152,9 +161,9 @@ int main(int argc, char *argv[])
             for(int k = 0; k<ceil(x*size); k++)
             {
 
-                old_x = (double)i * (double)width_ratio;
+                old_x = (double)i * width_ratio;
 
-                old_y = (double)k * (double)height_ratio;
+                old_y = (double)k * height_ratio;
 
 
                 xt[0]=floor(old_x);
@@ -189,91 +198,31 @@ int main(int argc, char *argv[])
 
         }
 
+       
+       if (size>=1)
+     {
         for(int i = 0; i<(x*size)*(y*size); i++)
         {
             
-            fprintf(fp2,"%.0lf ",img2[i].r);
-            fprintf(fp2,"%.0lf ",img2[i].g);
-            fprintf(fp2,"%.0lf ",img2[i].b);
+            fprintf(fp2,"%c",img2[i].r);
+            fprintf(fp2,"%c",img2[i].g);
+            fprintf(fp2,"%c",img2[i].b);
             
         }
+        
+        
+      }
 
-    }
 
     else
-    {
+   {
 
-
-
-        img2 = calloc(1,(x)*(y)*size*sizeof(pixel_rgb));
-
-
-
-        fprintf(fp2,"%c%d\n%.0lf %.0lf\n%d\n",format,format_2,ceil(size*x),ceil(y*size),max);
-
-
-
-        double old_x = 0;
-        double old_y = 0;
-
-        double width_ratio = (double)(x)/(double)(x*size);
-        double height_ratio = (double) (y)/(double) (y*size);
-
-        double A_r, A_g, A_b;
-        double B_r, B_g, B_b;
-
-        printf("\nwith_ratio is %lf\n", width_ratio);
-        printf("\nheight_ratio is %lf\n", height_ratio);
-
-        int index=0;
-        for(int i = 0; i<y*size; i++)
-        {
-            for(int k = 0; k<x*size; k++)
-            {
-
-                old_x = (double)i * (double)width_ratio;
-
-                old_y = (double)k * (double)height_ratio;
-
-
-                xt[0]=floor(old_x);
-                xt[1]=ceil(old_x);
-
-                yt[0]=floor(old_y);
-                yt[1]=ceil(old_y);
-
-                if (xt[0]==xt[1]) xt[1]++;
-                if (yt[0]==yt[1]) yt[1]++;
-
-                A_r = img[xt[0]][yt[0]].r * (xt[1]-old_x)/(xt[1]-xt[0]) + img[xt[1]][yt[0]].r * (old_x-xt[0])/(xt[1]-xt[0]);
-
-                B_r = img[xt[0]][yt[1]].r * (xt[1]-old_x)/(xt[1]-xt[0]) + img[xt[1]][yt[1]].r * (old_x-xt[0])/(xt[1]-xt[0]);
-
-                A_g = img[xt[0]][yt[0]].g * (xt[1]-old_x)/(xt[1]-xt[0]) + img[xt[1]][yt[0]].g * (old_x-xt[0])/(xt[1]-xt[0]);
-
-                B_g = img[xt[0]][yt[1]].g * (xt[1]-old_x)/(xt[1]-xt[0]) + img[xt[1]][yt[1]].g * (old_x-xt[0])/(xt[1]-xt[0]);
-
-                A_b = img[xt[0]][yt[0]].b * (xt[1]-old_x)/(xt[1]-xt[0]) + img[xt[1]][yt[0]].b * (old_x-xt[0])/(xt[1]-xt[0]);
-
-                B_b = img[xt[0]][yt[1]].b * (xt[1]-old_x)/(xt[1]-xt[0]) + img[xt[1]][yt[1]].b * (old_x-xt[0])/(xt[1]-xt[0]);
-
-
-
-
-                img2[index].r = (A_r * (yt[1]-old_y)/(yt[1]-yt[0]) + B_r * (old_y-yt[0])/(yt[1]-yt[0]));
-                img2[index].g = (A_g * (yt[1]-old_y)/(yt[1]-yt[0]) + B_g * (old_y-yt[0])/(yt[1]-yt[0]));
-                img2[index].b = (A_b * (yt[1]-old_y)/(yt[1]-yt[0]) + B_b * (old_y-yt[0])/(yt[1]-yt[0]));
-                index++;
-            }
-
-        }
-
-        for(int i = 0; i<x*y*size; i++)
+        for(int i = 0; i<floor(x*y*size); i++)
         {
             
-            fprintf(fp2,"%.0lf ",img2[i].r);
-            fprintf(fp2,"%.0lf ",img2[i].g);
-            fprintf(fp2,"%.0lf ",img2[i].b);
+            fprintf(fp2,"%c",img2[i].r);
+            fprintf(fp2,"%c",img2[i].g);
+            fprintf(fp2,"%c",img2[i].b);
             
         }
 
@@ -283,8 +232,6 @@ int main(int argc, char *argv[])
     //shared memory
     //das Selbe Spiel für P6
   
-
-    //removing some comments from the files and add other
 
     fclose(fp1);
     fclose(fp2);
